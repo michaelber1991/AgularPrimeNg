6ced08b75f6f9ae267f22f86e9e360b9dde94f91
michaelber1991 <michaelber1991@hotmail.com>
2024-08-04 20:15:35 +0200

    [Component]: table height and rows

--- src/app/presentation/components/prime-table/prime-table.component.model.ts
+++ src/app/presentation/components/prime-table/prime-table.component.model.ts
@@ -1,19 +1,30 @@
 export class PrimeTable {
 	title: string;
 	columns: PrimeTableColumn[];
+	rowsPerPageOptions: number[];
 	constructor(data: Partial<PrimeTable>) {
 		this.title = data.title || '';
 		this.columns = data.columns || [];
+		this.rowsPerPageOptions = data.rowsPerPageOptions || [10, 20, 50];
 	}
+}
+
+export enum PrimeTableColumnAlignFrozen {
+	Right = 'right',
+	Left = 'left'
 }
 
 export class PrimeTableColumn {
 	isFrozen: boolean;
 	property: string;
 	header: string;
+	minWidth: string;
+	alignFrozen: PrimeTableColumnAlignFrozen;
 	constructor(data: Partial<PrimeTableColumn>) {
 		this.isFrozen = data.isFrozen || false;
 		this.property = data.property || '';
 		this.header = data.header || '';
+		this.minWidth = data.minWidth || '';
+		this.alignFrozen = data.alignFrozen || PrimeTableColumnAlignFrozen.Left;
 	}
 }
