d3324c4f12718e7c73c34aa800d2cce35e51fcde
michaelber1991 <michaelber1991@hotmail.com>
2024-09-13 21:47:30 +0200

    [About]: clean about page

--- src/app/presentation/pages/about/about.component.ts
+++ src/app/presentation/pages/about/about.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { VirtualScrollTableComponent } from '@components/virtual-scroll-table/virtual-scroll-table.component';
 
 @Component({
 	selector: 'app-about',
 	standalone: true,
-	imports: [VirtualScrollTableComponent],
+	imports: [],
 	templateUrl: './about.component.html',
 	styleUrl: './about.component.scss'
 })
