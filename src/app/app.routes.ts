cf4b1fde66d4908b5a862502f39227f99984630f
michaelber1991 <michaelber1991@hotmail.com>
2025-04-19 16:40:14 +0200

    [Component]: login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,8 +6,6 @@ export const routes: Routes = [
 	{
 		path: AppRoutes.HOME,
 		loadChildren: () => import('./presentation/home/pages/home.routes').then((c) => c.routes)
-		// canActivate: [canActivateAuthRole],
-		// data: { role: ['EC_ROLE1'] } Keycloak roles
 	},
 	{
 		path: AppRoutes.LOGIN,
